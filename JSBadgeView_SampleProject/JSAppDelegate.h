21599aac4763e670546837404748c823765e7ca0
Javier Soto <javier.api@gmail.com>
2012-07-24 20:39:57 -0700

    Initial Commit

---
+++ JSBadgeView_SampleProject/JSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JSAppDelegate.h
+//  JSBadgeView_SampleProject
+//
+//  Created by Javier Soto on 7/24/12.
+//  Copyright (c) 2012 Javier Soto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JSBadgeView_SampleProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JSBadgeView_SampleProject
+//
+//  Created by Javier Soto on 7/24/12.
+//  Copyright (c) 2012 Javier Soto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JSAppDelegate class]));
+    }
+}
