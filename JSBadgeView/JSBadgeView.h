0eda7479ad206a5ace64bdb2423bceeb45a934c0
Vitaly <softdesigner.ua@gmail.com>
2015-03-27 14:13:46 +0200

    Fix a typo

--- JSBadgeView/JSBadgeView.h
+++ JSBadgeView/JSBadgeView.h
@@ -88,7 +88,7 @@ typedef NS_ENUM(NSUInteger, JSBadgeViewAlignment)
 @property (nonatomic, assign) CGRect frameToPositionInRelationWith UI_APPEARANCE_SELECTOR;
 
 /**
- * The minimal width of a badge circle. We need this to avoid elipse shapes when using small fonts.
+ * The minimum width of a badge circle. We need this to avoid elipse shapes when using small fonts.
  */
 @property (nonatomic, assign) CGFloat badgeMinWidth UI_APPEARANCE_SELECTOR;
 
